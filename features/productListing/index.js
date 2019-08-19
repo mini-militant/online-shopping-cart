49ba1ae0cbdbba9ee59584cdd6acb698c702f0fa
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-19 18:49:08 +0530

    1 file modified

--- features/productListing/index.js
+++ features/productListing/index.js
@@ -5,16 +5,12 @@ export default function ProductListing(props){
   return(
     <div>
     <p>ProductListing</p>
-    {console.log(props.products[0].name)}
-    
+    {console.log(props.products[0].name)}    
     {
       props.products.map(product=>{
-
         <ProductListItem  product={product}/>
       })
-    }
-      
-    
+    }   
     </div>
   )
 }
