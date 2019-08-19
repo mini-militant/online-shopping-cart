c38cbb99cc1c91bc57658263f5151d7f8007e5c9
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-19 19:31:22 +0530

    2 files modified

--- features/productListing/productListItem.js
+++ features/productListing/productListItem.js
@@ -3,8 +3,14 @@ import React from 'react'
 function ProductListItem(props){
   return(
     <div>
-    
-      <p>{props.product.name}</p>
+      <h1>{props.product.name}</h1>
+      <img
+        height={100}
+        title={props.product.name}
+        src={props.product.image}
+      />
+      <div>{props.product.description}</div>
+
     </div>
   )
 }
